8ed3585a8549d5402860f63b5a5efd1d42e0b2b5
Marc Heinrich <marc.heinrich@free.fr>
2012-11-07 22:44:10 +0100

    modif typeur
    Bon, j'ai modifié l'arbre de sytaxe abstraite, y'a maintenant un champ
    data  qui correspond au type de l'expression, quand le parser va générer
    l'arbre de syntaxe abstraite, il faudra qu'il initialise ce champ (avec
    nimporte quoi, ça a pas d'importance). J'ai pas pu faire deux types
    distincts pour l'abre de syntaxe abstraite, et l'arbre décoré, parce que
    sinon, y'a des conflits de notations entre les deux types, il faudrait
    soit préciser le module à chaque fois, soit paramétrer le type par le
    type des données que l'on veut mettre dans l'arbre. Dans les deux cas
    c'est un peu chiant. (mais si il faut vraiment le faire, c'est la
    deuxième solution la meilleure)
    voila, la fonction de typage fonctionne sur les expressions pour le
    moment, et modifie le champ dans l'arbre en place.
    Faut encore typer les instructions, et après les déclarations. A venir
    Faudra aussi faire des tests quand je serais motivé pour écrire des
    arbres à la main

--- Typeur.ml
+++ Typeur.ml
@@ -1,6 +1,5 @@
 open Definition
 
-
   let not_rec = function 
     | Int | Char | Typenull -> true
     | _ -> false
@@ -41,39 +40,6 @@ exception Type_error of position * string
 exception Argtype_error of position * string * (ttype list)
 (*== erreur à la position pos, la fonction (l'opérateur string) a eu les types de la liste en argument *)
 
-let rec string_of_type = function 
-	| Int -> "int" 
-	| Char -> "char"
-	| Void -> "void"
-	| Typenull -> "typenull"
-	| Struct s -> "struct : " ^s
-	| Union s -> "union : " ^s
-	| Pointer p -> "(" ^(string_of_type p)^")*"
-	
-let string_of_unop = function 
-	| PPleft -> "increment left operand"
-	| PPright -> "increment right operand"
-	| MMleft -> "decrement left operand"
-	| MMright -> "decrement right operand"
-	| Adr_get -> "unary '&' operand"
-	| Not -> "unary '!' operand"
-	| UPlus -> "unary '+' operand"
-	| UMinus -> "unary '-' operand"
-	
-let string_of_binop = function 
-	| Eq -> "binary '=='"
-	| Neq -> "binary '!='"
-	| Lt -> "binary '<'"
-	| Leq -> "binary '<='"
-	| Gt -> "binary '>'"
-	| Geq -> "binary '>='"
-	| BPlus -> "binary '+'"
-	| BMinus -> "binary '-'"
-	| Mul -> "binary '*'"
-	| Div -> "binary '/'"
-	| Mod -> "binary '%'"
-	| And -> "binary '&&'"
-	| Or -> "binary '||'"
 	
 (*dit si un opérateur binaire est un opérateur de comparaison ou non*)
 let compop = function 
@@ -90,121 +56,135 @@ le nom de l'identificateur (on ne peut pas lier plusieurs fois un id dans une ma
 let rec bien_forme env = function
 	| Void | Int | Char | Typenull -> true
 	| Pointer t -> bien_forme env t 
-	| Struct s -> begin try match SMap.find ("str_"^s) env with 
+	| Struct s -> begin try 
+		match SMap.find ("str_"^s) env with 
 		| Var _ | Fun _ | Uni _ -> false
 		| Str l -> List.fold_left (fun b -> fun t -> b&&(bien_forme env (fst t))) true l
 		with Not_found -> false
 		end
-	| Union s -> begin try match SMap.find ("uni_"^s) env with 
+	| Union s -> begin try 
+		match SMap.find ("uni_"^s) env with 
 		| Var _ | Fun _ | Str _ -> false
 		| Uni l -> List.fold_left (fun b -> fun t -> b&&(bien_forme env (fst t))) true l
 		with Not_found -> false
 		end
 
 		
-let rec getType env n = match n.exp with
-  | Entier 0 -> Typenull
-  | Entier _ -> Int
-  | Chaine _ -> Pointer(Char)
-  | Variable s -> begin try 
-		     let envt = SMap.find ("var_"^s) env in 
-		     match envt with 
-		     | Var t -> t
-		     | _ -> assert false
-    with Not_found -> raise (Type_error (n.pos,(s ^ " : undeclared")))
+let rec getType env n =  match n.exp with
+  | Entier 0 -> n.data <- Typenull
+  | Entier _ -> n.data <- Int
+  | Chaine _ -> n.data <- Pointer(Char)
+  | Variable s -> begin try
+	let envt = SMap.find ("var_"^s) env in
+	match envt with
+	| Var t -> n.data <- t
+	| _ -> assert false
+	with Not_found -> raise (Type_error (n.pos,(s ^ " : undeclared")))
   end
-  | Sizeof t -> if bien_forme env t then 
-      if not (equiv t Void) then Int else raise (Type_error (n.pos,("trying to get sizeof void")))
-    else raise (Type_error (n.pos,(string_of_type t ^ " est mal formé") ))
-      
-  | Unop (Adr_get, e) -> if lvalue e.exp then Pointer (getType env e)
+  | Sizeof t -> 
+	if bien_forme env t then
+	  if not (equiv t Void) then n.data <- Int 
+	  else raise (Type_error (n.pos,("trying to get sizeof void")))
+	else raise (Type_error (n.pos,(string_of_type t ^ " est mal formé") ))
+	  
+  | Unop (Adr_get, e) -> 
+    getType env e ;
+    if lvalue e.exp 
+    then n.data <- e.data
     else raise (Type_error (n.pos,"trying to get adress of expression with no left value"))
       
-  | Pointer_access(e) -> begin 
-    match getType env e with 
-    | Pointer t -> t
+  | Pointer_access(e) -> begin
+    getType env e ;
+    match e.data with
+    | Pointer t -> n.data <- t
     | _ -> raise (Type_error (n.pos,"pointer access of a non pointer expression"))
   end
     
-  | Access_field (e,x) -> 
-    begin 
-      match getType env e with 
-      | Union s -> 
-	let et = SMap.find ("uni_"^s) env in 
+  | Access_field (e,x) ->
+    begin
+      getType env e ;
+      match e.data with
+      | Union s ->
+	let et = SMap.find ("uni_"^s) env in
 	begin
-	  match et with 
-	  | Uni l -> begin 
-	    try 
-	      fst (List.find (fun (t,y) -> y = x) l);
+	  match et with
+	  | Uni l -> begin try
+			     n.data <- fst (List.find (fun (t,y) -> y = x) l);
 	    with Not_found -> raise (Type_error (n.pos,"l'identificateur " ^x^ "n'apparait pas dans " ^ "uni " ^ s ))
 	  end
-	  | _ -> assert false 
+	  | _ -> assert false
 	end
       | Struct s -> begin
-	let et = SMap.find ("str_"^s) env in 
-	match et with 
-	| Str l -> begin 
-	  try 
-	    fst (List.find (fun (t,y) -> y = x) l) ;
+	let et = SMap.find ("str_"^s) env in
+	match et with
+	| Str l -> begin try
+			   n.data <- fst (List.find (fun (t,y) -> y = x) l) ;
 	  with Not_found -> raise (Type_error (n.pos,"l'identificateur " ^x^ "n'apparait pas dans " ^ "str " ^ s ))
 	end
 	| _ -> assert false
       end
-      | _ -> raise (Type_error (n.pos,("request for member  '" ^x^ "' in something not a structure or union")))
+      | _ -> raise (Type_error (n.pos,("request for member '" ^x^ "' in something not a structure or union")))
     end
-    
-  | Assignement (e1,e2) -> 
-    if lvalue e1.exp then 
-      let t1 = getType env e1 and t2 = getType env e2 in
-      if equiv t1 t2 then t1 
-      else
-	raise (Type_error (n.pos,"incompatible types when assigning to type " ^ (string_of_type t1) ^" from type " ^ (string_of_type t2)))
+      
+  | Assignement (e1,e2) ->
+    if lvalue e1.exp then
+      begin getType env e1 ; getType env e2 ;
+	let t1 = e1.data and t2 = e2.data in
+	if equiv t1 t2 then n.data <- t1
+	else
+	  raise (Type_error (n.pos,"incompatible types when assigning to type " ^ (string_of_type t1) ^" from type " ^ (string_of_type t2)))
+      end
     else raise (Type_error (n.pos,"lvalue required as left operand of assignment" ))
       
-  | Unop(op,e) when (op= PPleft)||(op =PPright)||(op = MMleft)||(op = MMright) -> 
-    if lvalue e.exp then 
-      let t = getType env e in 
-      if num t then t
-      else raise (Argtype_error (n.pos,(string_of_unop op),[t]))
+      
+  | Unop(op,e) when (op= PPleft)||(op =PPright)||(op = MMleft)||(op = MMright) ->
+    if lvalue e.exp then
+      begin
+	getType env e ;
+	let t = e.data in
+	if num t then n.data <- t
+	else raise (Argtype_error (n.pos,(string_of_unop op),[t]))
+      end
     else raise (Type_error (n.pos,"lvalue required as left operand of assignment" ))
       
-  | Unop(op,e) when (op = UPlus)||(op = UMinus) -> let t = getType env e in 
-						   if equiv t Int 
-						   then Int 
-						   else raise (Argtype_error (n.pos,(string_of_unop op),[t]))
-						     
-  | Unop(Not,e) -> let t = getType env e in if num t then Int else raise (Argtype_error (n.pos,(string_of_unop Not),[t]))
+  | Unop(op,e) when (op = UPlus)||(op = UMinus) -> 
+    getType env e ;
+    let t = e.data in
+    if equiv t Int
+    then n.data <- Int
+    else raise (Argtype_error (n.pos,(string_of_unop op),[t])) 
+      
+  | Unop(Not,e) -> getType env e ; let t = e.data in if num t then n.data <- Int else raise (Argtype_error (n.pos,(string_of_unop Not),[t]))
   | Unop (_,_) -> assert false (*tous les autres cas ont été traités avant, c'est pour éviter un warning*)
     
-  | Binop(op,e1,e2) when compop op -> let t1 = getType env e1 and t2 = getType env e2 in 
-				      if (equiv t1 t2)&&(num t1) then Int 
-				      else raise (Argtype_error (n.pos,(string_of_binop op),[t1;t2]))
-					
-  | Binop(op,e1,e2) -> let t1 = getType env e1 and t2 = getType env e2 in 
-		       if (equiv t1 t2)&&(num t1) then Int 
-		       else 
-			 if (ispointer t1)&&(equiv t2 Int)&&((op = BPlus)||(op = BMinus)) 
-			 then t1
-			 else 
-			   if (ispointer t2)&&(equiv t1 Int)&&(op = BPlus)
-			   then t2
-			   else
-			     if (ispointer t1)&&(equiv t1 t2)&&(op = BMinus)
-			     then Int
-			     else raise (Argtype_error (n.pos,(string_of_binop op),[t1;t2])) (*risque d'erreur important*)
-			       
-  | Call(f,l) -> begin try 
-			 match SMap.find ("fun_"^f) env with 
-			 | Fun (t,lt) -> let lt'= List.map (getType env) l in 
-					 let b = List.fold_left2 (fun b -> fun t1 -> fun t2 -> b&&(equiv t1 t2)) true lt lt' in 
-					 if b then t else raise (Argtype_error (n.pos,("function '" ^f^"'"),lt'))
-			 | _ -> assert false
-			   
+  | Binop(op,e1,e2) when compop op -> getType env e1 ; getType env e2 ; 
+    let t1 = e1.data and t2 = e2.data in
+    if (equiv t1 t2)&&(num t1) then n.data <- Int
+    else raise (Argtype_error (n.pos,(string_of_binop op),[t1;t2]))
+      
+  | Binop(op,e1,e2) -> getType env e1 ; getType env e2 ;
+    let t1 = e1.data and t2 = e2.data in
+    if (equiv t1 t2)&&(num t1) then n.data <-Int
+else
+      if (ispointer t1)&&(equiv t2 Int)&&((op = BPlus)||(op = BMinus))
+      then n.data <- t1
+      else
+	if (ispointer t2)&&(equiv t1 Int)&&(op = BPlus)
+	then n.data <- t2
+	else
+	  if (ispointer t1)&&(equiv t1 t2)&&(op = BMinus)
+	  then n.data <- Int
+	  else raise (Argtype_error (n.pos,(string_of_binop op),[t1;t2])) (*risque d'erreur important*)
+	    
+  | Call(f,l) -> begin try
+			 match SMap.find ("fun_"^f) env with
+| Fun (t,lt) -> List.iter (getType env) l ;
+  let lt'= List.map (fun e -> e.data) l in
+  let b = List.fold_left2 (fun b -> fun t1 -> fun t2 -> b&&(equiv t1 t2)) true lt lt' in
+  if b then n.data <- t else raise (Argtype_error (n.pos,("function '" ^f^"'"),lt'))
+| _ -> assert false
+  
     with Not_found -> raise (Type_error (n.pos,"unbound function '"^f^"'"))
-  end 
+    | Invalid_argument _ -> raise (Type_error (n.pos, "wrong number of argument for function '"^f^"'"))
+  end
     
-		 
-		 
-		 
-		
-		
