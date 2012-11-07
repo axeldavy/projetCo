69f6818c3979ec5a8ade09941925778c30ea3b92
Marc Heinrich <marc.heinrich@free.fr>
2012-11-07 22:45:13 +0100

    oubli

--- Definition.ml
+++ Definition.ml
@@ -1,9 +1,8 @@
-type position = int*int(*TODO: comment on récupère la poisition des différents éléments du fichier, 
-cf menhir?*)
+type position = int*int
+and etiquette = ttype
+and ident = string
 
-type ident = string
-
-type decl = 
+and decl = 
 	|Dvar of decl_vars
 	|Dt of  decl_typ 
 	|Df of ttype * ident * ((ttype * ident) list) * bloc
@@ -30,16 +29,53 @@ and expr =
   |Binop of bop * node * node 
   |Sizeof of ttype 
   
-and node = {exp : expr ; pos : position} 
+and node = {exp : expr ; pos : position; mutable data : etiquette} 
 (*type noeud de l'arbre, pos correspond à la position dans le fichier*)
   
 and instruction =
+  |Empty
   |Expr of expr 
-  |If of expr * bloc * bloc
-  |While of expr * bloc
-  |For of (expr list * (expr option) * expr list)
+  |If of expr * instruction * (instruction option)
+  |While of expr * instruction
+  |For of (expr list * (expr option) * instruction)
   |Return of (expr option)
+  |Bloc of bloc
   
 
 and bloc = (decl_vars list) * (expr list)
+
+	let rec string_of_type = function 
+	| Int -> "int" 
+	| Char -> "char"
+	| Void -> "void"
+	| Typenull -> "typenull"
+	| Struct s -> "struct : " ^s
+	| Union s -> "union : " ^s
+	| Pointer p -> "(" ^(string_of_type p)^")*"
+	
+let string_of_unop = function 
+	| PPleft -> "increment left operand"
+	| PPright -> "increment right operand"
+	| MMleft -> "decrement left operand"
+	| MMright -> "decrement right operand"
+	| Adr_get -> "unary '&' operand"
+	| Not -> "unary '!' operand"
+	| UPlus -> "unary '+' operand"
+	| UMinus -> "unary '-' operand"
+	
+let string_of_binop = function 
+	| Eq -> "binary '=='"
+	| Neq -> "binary '!='"
+	| Lt -> "binary '<'"
+	| Leq -> "binary '<='"
+	| Gt -> "binary '>'"
+	| Geq -> "binary '>='"
+	| BPlus -> "binary '+'"
+	| BMinus -> "binary '-'"
+	| Mul -> "binary '*'"
+	| Div -> "binary '/'"
+	| Mod -> "binary '%'"
+	| And -> "binary '&&'"
+	| Or -> "binary '||'"
+
 
