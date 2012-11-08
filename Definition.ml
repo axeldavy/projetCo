c208d9a055bd1659abf3172b6b3b56e09b7bf775
Marc Heinrich <marc.heinrich@free.fr>
2012-11-08 20:48:13 +0100

    rechangement définition
    Bon, j'ai rechangé l'arbre de syntaxe abstraite, enfin, j'ai plustôt
    redéfini un type d'arbre pour le typage.
    La seule modification dans l'arbre de syntaxe abstraite par rapport à sa
    version orginale est l'ajout de l'expression Char of char, je pense
    qu'il le faudra, puisque on accepte les caractères comme constantes
    
    Pour l'arbre de typage, y'a pas mal de modif:
    .suppression des for
    .définition d'un type mtype où les identificateurs de structures sont
    remplacés par des enregistrements qui contiennent diverses informations
    qui pourront être utiles à la fois pour le typage et pour la production
    de code après (et même chose pour les variables)
    et ajout d'un truc pour distinguer les variables locales et globales.
    Et c'est à peu près tout
    à cause des InterENS, je pourrais pas bosser ce week end. il faut encore
    que je remodifie ma fonction de typage des expressions, c'est en cours,
    et ce sera pas fini ce soir.

--- Definition.ml
+++ Definition.ml
@@ -1,50 +1,15 @@
-type position = int*int
-and etiquette = ttype
-and ident = string
 
-and decl = 
-	|Dvar of decl_vars
-	|Dt of  decl_typ 
-	|Df of ttype * ident * ((ttype * ident) list) * bloc
+type position = Lexing.position
+ 
+type ident = string
 
-and decl_typ = Dstruct of ident * decl_vars list | Dunion of ident * decl_vars list
+type ttype = Void | Int | Char | Struct of ident | Union of ident (* ajout? *) | Pointer of ttype | Typenull
+type unop = PPleft | PPright | MMleft | MMright | Adr_get | Not | UMinus | UPlus  
 
-and decl_vars = ttype * (ident list)
+type  bop = Eq | Neq | Lt | Leq | Gt | Geq | BPlus | BMinus | Mul | Div | Mod | And | Or
 
-and ttype = Void | Int | Char | Struct of ident | Union of ident (* ajout? *) | Pointer of ttype | Typenull
 
-and unop = PPleft | PPright | MMleft | MMright | Adr_get | Not | UMinus | UPlus  
-
-and bop = Eq | Neq | Lt | Leq | Gt | Geq | BPlus | BMinus | Mul | Div | Mod | And | Or
-
-and expr = 
-  |Entier of int
-  |Chaine of string
-  |Variable of ident 
-  |Pointer_access of node 
-  |Access_field of node * ident 
-  |Assignement of node * node
-  |Call of ident * (node list) 
-  |Unop of unop * node 
-  |Binop of bop * node * node 
-  |Sizeof of ttype 
-  
-and node = {exp : expr ; pos : position; mutable data : etiquette} 
-(*type noeud de l'arbre, pos correspond à la position dans le fichier*)
-  
-and instruction =
-  |Empty
-  |Expr of expr 
-  |If of expr * instruction * (instruction option)
-  |While of expr * instruction
-  |For of (expr list * (expr option) * instruction)
-  |Return of (expr option)
-  |Bloc of bloc
-  
-
-and bloc = (decl_vars list) * (expr list)
-
-	let rec string_of_type = function 
+let rec string_of_type = function 
 	| Int -> "int" 
 	| Char -> "char"
 	| Void -> "void"
@@ -78,4 +43,105 @@ let string_of_binop = function
 	| And -> "binary '&&'"
 	| Or -> "binary '||'"
 
+	
+(*module contenant le type de l'arbre de syntaxe abstraite*)	
+module Ast = struct
+type decl = 
+	|Dvar of decl_vars
+	|Dt of  decl_typ 
+	|Df of decl_fun
+	
+and decl_fun = {decfun : ttype * ident * ((ttype * ident) list) * bloc ; decfun_pos : position }
 
+and decl_typ = {dectype : dec_t ; dectype_pos : position}
+
+and dec_t = Dstruct of ident * decl_vars list | Dunion of ident * decl_vars list
+
+and decl_vars = {decvar : ttype * (ident list) ; decvar_pos : position }
+
+and expr = 
+  |Character of char
+  |Entier of int
+  |Chaine of string
+  |Variable of ident 
+  |Pointer_access of expression 
+  |Access_field of expression * ident 
+  |Assignement of expression * expression
+  |Call of ident * (expression list) 
+  |Unop of unop * expression 
+  |Binop of bop * expression * expression 
+  |Sizeof of ttype 
+ 
+ and expression = {exp : expr ; exp_pos : position}
+  
+and instr =
+  |Empty
+  |Expression of  expression
+  |If of expression * instruction * (instruction option)
+  |While of expression * instruction
+  |For of (expression list * (expression option) * instruction)
+  |Return of (expression option)
+  |Bloc of bloc
+  
+and instruction = {instr : instr ; instr_pos : position}
+ 
+and bloc = {bloc : (decl_vars list) * (instruction list) ; bloc_pos : position}
+
+end
+
+
+(*module contenant le type de l'arbre décoré*)
+module Ast_Type = struct 
+
+type gvar = {gv_name : ident ; gv_type : mtype}	
+and lvar = {lv_name : ident ; mutable lv_loc : int ; lv_type : mtype}
+(*loc : localisation de la variable locale sur la pile, sera utilisé plus tard*)
+and var = TGvar of gvar | TLvar of lvar
+and func = {f_name : ident ; f_type : mtype ; f_arg : (ident * mtype) list}	
+and stru = {s_name : ident ; s_content : (ident * mtype) list ; s_size : int}
+and uni = {u_name : ident ; u_content : (ident * mtype) list ; u_size : int}
+and mtype = TVoid | TInt | TChar | TStruct of stru | TUnion of uni | TPointer of mtype | TTypenull
+
+type decl = 
+	|TDvar of decl_vars
+	|TDt of  decl_typ 
+	|TDf of decl_fun
+	
+and decl_fun = {tfun : func ; tcontent : bloc ; tdecfun_pos : position }
+and decl_typ = {tdectype : dec_t ; tdectype_pos : position}
+and dec_t = TDstruct of stru | TDunion of uni
+and decl_vars = {decvar : var list ; decvar_pos : position }
+
+and expr = 
+  |TCharacter of char
+  |TEntier of int
+  |TChaine of string
+  |TVariable of var
+  |TPointer_access of expression 
+  |TAccess_field of expression * ident 
+  |TAssignement of expression * expression
+  |TCall of func * (expression list) 
+  |TUnop of unop * expression 
+  |TBinop of bop * expression * expression 
+  |TSizeof of mtype 
+ 
+ and expression = {texp : expr ; texp_pos : position; texp_type : mtype}
+  
+and instr =
+  |TEmpty
+  |TExpression of  expression
+  |TIf of expression * instruction * (instruction option)
+  |TWhile of expression * instruction
+  |TReturn of (expression option)
+  |TBloc of bloc
+  
+and instruction = {tinstr : instr ; tinstr_pos : position }
+ 
+and bloc = {tbloc : (decl_vars list) * (instruction list) ; tbloc_pos : position}
+
+
+exception Type_error of position * string
+exception Argtype_error of position * string * (mtype list)
+(*== erreur à la position pos, la fonction (l'opérateur string) a eu les types de la liste en argument *)	
+	
+end 
